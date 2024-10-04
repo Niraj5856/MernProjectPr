31c0fdfb374ad9d3b26e4bc34824233823a6f4e8
niraj5856 <nirajroy5856@gmail.com>
2024-10-04 17:28:43 +0530

    createtodo

--- todoController/TodoCreate.js
+++ todoController/TodoCreate.js
@@ -2,8 +2,20 @@ const TodoModel = require("../models/TodoModel")
 
 exports.createTodo = async (req, res) => {
     try {
+        const {name ,phone ,description ,address}=req.body;
+        const response=await TodoModel.create({name ,phone ,description ,address});
+
+        return res.status(201).json({
+            success:true ,
+            data:response ,
+            message:"sucessfully created todo"
+        })
 
     } catch (error) {
+        return res.status(500).json({
+            success:false ,
+            message:error.message
+        })
 
     }
 }
